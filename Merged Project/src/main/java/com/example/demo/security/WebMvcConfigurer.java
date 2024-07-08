ca3f08aabc15b77994e83b63952552a24e7c5e5c
AnhNQ-FPT <anhnqhe181459@fpt.edu.vn>
2024-07-08 15:04:48 +0700

    uploaded stuff

--- Merged Project/src/main/java/com/example/demo/security/WebMvcConfigurer.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.security;
-
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
-
-public interface WebMvcConfigurer {
-    void addResourceHandlers(ResourceHandlerRegistration registry);
-}
