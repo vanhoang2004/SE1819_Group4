75736ed69432a39ed8c97e8ff4b9912a5e37a1a7
MFUN11 <163836985+MFUN11@users.noreply.github.com>
2024-07-17 15:07:38 +0700

    Delete Merged Project/merged-all directory

--- Merged Project/merged-all/src/main/java/com/example/demo/data/StudentTakenPraticeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.data;
-
-import com.example.demo.entity.StudentTakenPractice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentTakenPraticeRepository extends JpaRepository<StudentTakenPractice, Integer> {
-	
-}
