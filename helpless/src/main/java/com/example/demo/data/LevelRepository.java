16734af7385b0e0bc1a9e4bb9285c171a142963c
ShiNiDuiMa <drngocnguyen66@gmail.com>
2024-07-18 14:43:41 +0700

    commit

--- helpless/src/main/java/com/example/demo/data/LevelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.data;
-
-import com.example.demo.entity.Level;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LevelRepository extends JpaRepository<Level, Integer> {
-
-}
